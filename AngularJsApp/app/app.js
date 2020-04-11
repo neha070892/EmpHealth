de0b3e0a2492eefdcddc9cb2a9c77e7cff49140e
Neha Chawla <nchawla@bravurasolutions.com>
2020-04-11 14:57:20 +0530

    Added 4th commit-Collapse grids

--- AngularJsApp/app/app.js
+++ AngularJsApp/app/app.js
@@ -2,4 +2,9 @@
 
 app.controller("myController", function ($scope) {
     $scope.path = "images/health-is-wealth.jpg";
+    $scope.active = true;
+    $scope.active1 = true;
+    $scope.active2 = true;
+    $scope.active3 = true;
+    $scope.active4 = true;
 });
