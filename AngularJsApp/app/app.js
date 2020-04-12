be5f2bc228c2b754e489341f5c1c23f6bab498b8
Neha Chawla <nchawla@bravurasolutions.com>
2020-04-12 19:30:38 +0530

    Added ptext to css and html[p text in centre]

--- AngularJsApp/app/app.js
+++ AngularJsApp/app/app.js
@@ -2,10 +2,11 @@
 
 app.controller("myController", function ($scope) {
     $scope.path = "images/health-is-wealth.jpg";
-    $scope.pathpic = "images/myemp.JPG";
+    $scope.logo = "images/myemp.JPG";
     $scope.active = true;
     $scope.active1 = true;
     $scope.active2 = true;
     $scope.active3 = true;
     $scope.active4 = true;
+    $scope.active5 = true;
 });
